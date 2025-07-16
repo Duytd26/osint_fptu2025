5743f8c263547504caa3f7bcb6b25069541b8b97
Duy Tran <trandinhduy26062006@gmail.com>
2025-07-16 07:56:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -192,10 +192,17 @@ app.get('/leaderboard', isAuthenticated, (req, res) => {
                 return { name: row.name, finish_time: "" };
             }
 
-            const localTime = dateUTC.toLocaleString('vi-VN', {
+            const localTime = new Intl.DateTimeFormat('sv-SE', {
                 timeZone: 'Asia/Ho_Chi_Minh',
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
                 hour12: false
-            });
+            }).format(dateUTC).replace(' ', 'T');
+
 
             return {
                 name: row.name,
